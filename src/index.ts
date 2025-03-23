edf6316e3bd45057e9355da9699a7a8d22783ae6
unbywyd <unbywyd@gmail.com>
2025-03-23 15:14:29 +0200

    +

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import "reflect-metadata";
 import cron from 'node-cron';
 import type { AppContext, AppPlugin } from "@tsdiapi/server";
-
+import { Container } from "typedi";
+import type { FastifyInstance } from "fastify";
+declare module "fastify" {
+    interface FastifyInstance {
+        cron: typeof cron;
+    }
+}
 export type PluginOptions = {
     autoloadGlobPath: string;
 }
@@ -28,7 +34,6 @@ export interface ManualCronTask {
 
 let CRON_TASKS: CronTaskMetadata[] = [];
 
-
 /**
  * @CronTask - Decorator for cron tasks
  * @param schedule - cron schedule
@@ -52,24 +57,23 @@ class App implements AppPlugin {
     cronTasks: CronTaskMetadata[] = [];
     manualCronTasks: Map<string, ManualCronTask> = new Map();
     startCronTasks(context: AppContext) {
-        const container = context.container;
         for (const task of CRON_TASKS) {
-            const instance = container.get(task.target.constructor);
+            const instance = Container.get(task.target.constructor);
             cron.schedule(task.schedule, async () => {
                 try {
                     const method = (instance as any)[task.methodName];
                     if (typeof method === 'function') {
                         await method.call(instance);
-                        this.context.logger.info(`Cron task "${task.options?.name || task.methodName}" executed successfully.`);
+                        this.context.fastify.log.info(`Cron task "${task.options?.name || task.methodName}" executed successfully.`);
                     } else {
-                        this.context.logger.warn(`Method "${task.methodName}" is not a function.`);
+                        this.context.fastify.log.warn(`Method "${task.methodName}" is not a function.`);
                     }
                 } catch (error) {
-                    this.context.logger.error(`Error executing cron task "${task.options?.name || task.methodName}": ${error.message}`);
+                    this.context.fastify.log.error(`Error executing cron task "${task.options?.name || task.methodName}": ${error.message}`);
                 }
             });
             const name = task.options?.name || task.methodName;
-            this.context.logger.info(`Cron task "${name}" scheduled with: ${task.schedule}`);
+            this.context.fastify.log.info(`Cron task "${name}" scheduled with: ${task.schedule}`);
         }
     }
     constructor(config?: PluginOptions) {
@@ -79,28 +83,30 @@ class App implements AppPlugin {
     }
     async onInit(ctx: AppContext) {
         this.context = ctx;
+        if (!ctx.fastify.hasDecorator("cron")) {
+            ctx.fastify.decorate('cron', cron);
+        }
     }
-    async registerManualCronTask(data: ManualCronTask) {
+    async addCronTask(data: ManualCronTask) {
         const { id, schedule } = data;
         if (this.manualCronTasks.has(id)) {
-            this.context.logger.error(`Manual cron task with id "${id}" already exists.`);
+            this.context.fastify.log.error(`Manual cron task with id "${id}" already exists.`);
             return;
         }
 
         this.manualCronTasks.set(id, data);
-
         cron.schedule(schedule, async () => {
             try {
                 const manualTask = this.manualCronTasks.get(id);
                 if (manualTask?.task) {
                     await manualTask.task(this.context);
-                    this.context.logger.info(`Manual cron task "${id}" executed successfully.`);
+                    this.context.fastify.log.info(`Manual cron task "${id}" executed successfully.`);
                 }
             } catch (error) {
-                this.context.logger.error(`Error executing manual cron task "${id}": ${error.message}`);
+                this.context.fastify.log.error(`Error executing manual cron task "${id}": ${error.message}`);
             }
         });
-        this.context.logger.info(`Manual cron task "${id}" scheduled with: ${schedule}`);
+        this.context.fastify.log.info(`Manual cron task "${id}" scheduled with: ${schedule}`);
     }
 
     async afterStart(ctx: AppContext) {
